c620ae69605b83fe8c346a16273d95228bdf11af
Prizmin <helloworldpy103@gmail.com>
2023-12-21 15:51:31 -0800

    added pulsar and vs support.

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ RPC = Presence(client_id)
 RPC.connect()
 
 while True: #infinite loop
-    # Check if VSCode or Steam is running
+    # Check if VSCode, Visual Studio, Pulsar, or Steam is running
     vscode_running = "Code.exe" in (p.name() for p in psutil.process_iter())
+    visual_studio_running = "devenv.exe" in (p.name() for p in psutil.process_iter())
+    pulsar_running = "Pulsar.exe" in (p.name() for p in psutil.process_iter())
     steam_running = "steam.exe" in (p.name() for p in psutil.process_iter())
 
     # Get the title of the currently active window
@@ -20,16 +22,22 @@ while True: #infinite loop
     details = "just chillin'"
     state = "state of chillness"
 
-    # Update details and state based on whether VSCode or Steam is running
+    # Update details and state based on whether VSCode, Visual Studio, Pulsar, or Steam is running
     if vscode_running and "Visual Studio Code" in active_window_title:
-        details = "codin' shit"
-        state = "state of programming"
+        details = "workin' in vscode"
+        state = active_window_title if active_window_title else "state of programming"
+    elif visual_studio_running and "Visual Studio" in active_window_title:
+        details = "workin' in vs"
+        state = active_window_title if active_window_title else "state of programming"
+    elif pulsar_running and "Pulsar" in active_window_title:
+        details = "workin' in pulsar/atom"
+        state = active_window_title if active_window_title else "state of programming"
     elif steam_running and "Steam" in active_window_title:
         details = "gamin'"
         state = "state of gamin' rage"
     elif vscode_running and steam_running:
-        details = active_window_title if active_window_title else "Multitaskin'"
-        state = "Multitaskin' on windows"
+        details = active_window_title if active_window_title else "workin' a lot"
+        state = "chillin' on windows"
 
     RPC.update(
         large_image = "armature",
@@ -39,6 +47,5 @@ while True: #infinite loop
         details = details,
         state = state,
         start = start,
-        buttons = [{"label": "GitHub", "url": "https://github.com/armature64"}]
     )
-    time.sleep(15)
+    time.sleep(30)
